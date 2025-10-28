c0783ea1156a75e043b544c9fbc185a0022a2e2c
Francesco-Colombari <francesco.colombari@aldini.istruzioneer.it>
2025-10-28 08:34:04 +0000

    LEGGISCRIVIFILE

--- leggiscrivifile/src/App.java
+++ leggiscrivifile/src/App.java
@@ -1,24 +1,40 @@
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException; 
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        // prima scrivo dentro un file
-        FileWriter myWriter = new FileWriter("filename.txt");
-        for (int i=0; i<100; i++){
-            myWriter.write("Questa è una prima frase scritta in un file");
-        }
-        myWriter.close();
-        System.out.println("Ok, ho scritto sul file!");
+    public static void main(String[] args) {
+        Scanner scannerInput = new Scanner(System.in);
+        
+        System.out.print("Inserisci il nome del file di input: ");
+        String nomeFileIn = scannerInput.nextLine();
+        
+        System.out.print("Inserisci il nome del file di output: ");
+        String nomeFileOut = scannerInput.nextLine();
+            scannerInput.close(); 
+        
+        // garantisce che gli oggetti vengano chiusi automaticamente, anche in caso di eccezioni.
+        try (
+            Scanner leggiFile = new Scanner(new File(nomeFileIn));
+            
+            FileWriter myWriter = new FileWriter(nomeFileOut)
+        ) {
+            
+            int conteggioRighe = 0;
+            
+            while (leggiFile.hasNextLine()) {
+                String riga = leggiFile.nextLine();
+                
+                myWriter.write(riga);
+                myWriter.write(System.lineSeparator()); 
+                conteggioRighe++;
+            }
+            System.out.println("Il file è stato copiato");
+            System.out.println("Copiato " + conteggioRighe + " righe da '" + nomeFileIn + "' a '" + nomeFileOut + "'.");
 
-        //ora leggo cosa c'è nel file
-        File f = new File("filename.txt");
-        Scanner leggi= new Scanner(f);
-        while (leggi.hasNextLine()) {
-            String s= leggi.nextLine();
-            System.out.println(s);
+        } catch (IOException e) {
+            System.err.println("\n❌ Errore durante l'operazione sui file: " + e.getMessage());
         }
-        leggi.close();
     }
 }
